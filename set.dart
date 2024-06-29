f77260f3799d9f1a9029da16f41903882f5bfea7
Devanarayanan.S.B <devan20034014@gmail.com>
2024-06-29 09:04:48 +0530

    new
    set

---
+++ set.dart
@@ -0,0 +1,9 @@
+void main() {
+  String y = "Arjun";
+  String Y = "devan";
+  Set<String> x = {"akash", "Arjun", y, Y};
+  Set<int> a = {1, 2, 3, 4, 5};
+  Set<int> b = {4, 5, 6, 7};
+  print(x);
+  print(a.union(b));
+}
